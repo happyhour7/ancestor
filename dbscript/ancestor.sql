16d76a4c98b203acd4500e935958b3e7dbe3cda3
happyhour7 <happyhour7@163.com>
2015-07-21 18:33:38 +0800

    sqlscript

---
+++ dbscript/ancestor.sql
@@ -0,0 +1,98 @@
+﻿# Host: 10.20.107.253  (Version: 5.1.73-community)
+# Date: 2015-07-21 18:33:21
+# Generator: MySQL-Front 5.3  (Build 4.214)
+
+/*!40101 SET NAMES utf8 */;
+
+#
+# Structure for table "admin"
+#
+
+DROP TABLE IF EXISTS `admin`;
+CREATE TABLE `admin` (
+  `Id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `logintime` datetime DEFAULT NULL,
+  `userid` varchar(255) DEFAULT NULL,
+  `userphoto` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`Id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+#
+# Data for table "admin"
+#
+
+INSERT INTO `admin` VALUES (1,'管理员','admin',NULL,'admin',NULL);
+
+#
+# Structure for table "files"
+#
+
+DROP TABLE IF EXISTS `files`;
+CREATE TABLE `files` (
+  `Id` int(11) NOT NULL AUTO_INCREMENT,
+  `filetype` int(11) DEFAULT NULL,
+  PRIMARY KEY (`Id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='帖子';
+
+#
+# Data for table "files"
+#
+
+
+#
+# Structure for table "friends"
+#
+
+DROP TABLE IF EXISTS `friends`;
+CREATE TABLE `friends` (
+  `Id` int(11) NOT NULL AUTO_INCREMENT,
+  `userid` int(11) DEFAULT NULL,
+  `friendid` int(11) DEFAULT NULL,
+  PRIMARY KEY (`Id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+#
+# Data for table "friends"
+#
+
+
+#
+# Structure for table "talk"
+#
+
+DROP TABLE IF EXISTS `talk`;
+CREATE TABLE `talk` (
+  `Id` int(11) NOT NULL AUTO_INCREMENT,
+  `senduserid` int(11) DEFAULT NULL,
+  `receiveruserid` int(11) DEFAULT NULL,
+  `msg` varchar(500) DEFAULT NULL,
+  `sendtime` datetime DEFAULT NULL,
+  PRIMARY KEY (`Id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+#
+# Data for table "talk"
+#
+
+
+#
+# Structure for table "users"
+#
+
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE `users` (
+  `Id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(50) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `sex` int(11) DEFAULT '0',
+  `usertype` int(11) DEFAULT '0' COMMENT '0:普通用户；1：广告主；2：咨询师',
+  `score` int(11) DEFAULT NULL,
+  PRIMARY KEY (`Id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+#
+# Data for table "users"
+#
+
