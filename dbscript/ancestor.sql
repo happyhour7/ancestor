83e1f9d2c09e0489f353c9b45c022e36714325f5
happyhour7@163.com <happyhour7@163.com>
2015-08-09 20:53:42 +0800

    home

--- dbscript/ancestor.sql
+++ dbscript/ancestor.sql
@@ -1,5 +1,5 @@
-﻿# Host: 10.20.107.253  (Version: 5.1.73-community)
-# Date: 2015-07-21 18:33:21
+﻿# Host: localhost  (Version: 5.5.20)
+# Date: 2015-08-09 20:53:23
 # Generator: MySQL-Front 5.3  (Build 4.214)
 
 /*!40101 SET NAMES utf8 */;
@@ -32,14 +32,34 @@ INSERT INTO `admin` VALUES (1,'管理员','admin',NULL,'admin',NULL);
 DROP TABLE IF EXISTS `files`;
 CREATE TABLE `files` (
   `Id` int(11) NOT NULL AUTO_INCREMENT,
-  `filetype` int(11) DEFAULT NULL,
+  `secretType` varchar(45) DEFAULT NULL,
+  `secretSubType` varchar(45) DEFAULT NULL,
+  `secretGrandSubType` varchar(45) DEFAULT NULL,
+  `secretLimit` int(11) DEFAULT NULL,
+  `secretMainType` varchar(45) DEFAULT NULL,
+  `secretHope` varchar(45) DEFAULT NULL,
+  `secretCity` varchar(45) DEFAULT NULL,
+  `secretDate` varchar(45) DEFAULT NULL,
+  `secretKeyWord` varchar(45) DEFAULT NULL,
+  `secretTitle` varchar(45) DEFAULT NULL,
+  `secretBackground` varchar(45) DEFAULT NULL,
+  `secretContent` varchar(45) DEFAULT NULL,
+  `secretKnown` varchar(45) DEFAULT NULL,
+  `othername` varchar(45) DEFAULT NULL,
+  `othersex` int(11) DEFAULT NULL,
+  `otherage` varchar(45) DEFAULT NULL,
+  `otherBuildName` varchar(45) DEFAULT NULL,
+  `otheraddress` varchar(200) DEFAULT NULL,
+  `secretPrice` int(11) DEFAULT NULL,
+  `owner` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`Id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='帖子';
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8 COMMENT='帖子';
 
 #
 # Data for table "files"
 #
 
+INSERT INTO `files` VALUES (9,'我爱','友情','',0,'WO的秘密','求祝福','南京','2015-08-04','关键词','标题','背景','正文','感悟','undefined',0,'undefined','undefined','undefined',0,NULL),(10,'我恨','爱人','',0,'WO的秘密','求祝福','南京','2015-08-04','这是第一个秘密哦','这是第一个秘密哦','这是第一个秘密哦','这是第一个秘密哦','这是第一个秘密哦','',0,'','','',0,'test');
 
 #
 # Structure for table "friends"
@@ -89,10 +109,18 @@ CREATE TABLE `users` (
   `sex` int(11) DEFAULT '0',
   `usertype` int(11) DEFAULT '0' COMMENT '0:普通用户；1：广告主；2：咨询师',
   `score` int(11) DEFAULT NULL,
+  `email` varchar(45) DEFAULT NULL,
+  `cityname` varchar(45) DEFAULT NULL,
+  `qq` varchar(45) DEFAULT NULL,
+  `phone` varchar(45) DEFAULT NULL,
+  `userPhoto` varchar(45) DEFAULT 'demo_logo.png',
+  `address` varchar(200) DEFAULT NULL,
+  `mark` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`Id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=71 DEFAULT CHARSET=utf8;
 
 #
 # Data for table "users"
 #
 
+INSERT INTO `users` VALUES (1,'test','test',0,1,10,'happyhour7@163.com','南京','251619896','13813994215','0.06278088735416532.jpg','asdfasdf2323','asdfasdfasdfas'),(67,'william wang','123456',1,1,10,'happyhour7@163.com','南京','251619896','13813994215','demo_logo.png',NULL,NULL),(68,'william wang','123456',1,1,10,'happyhour7@163.com','南京','251619896','13813994215','demo_logo.png',NULL,NULL),(69,'william wang','123456',1,1,10,'happyhour7@163.com','南京','251619896','13813994215','demo_logo.png',NULL,NULL),(70,'william wang','123456',1,1,10,'happyhour7@163.com','南京','251619896','13813994215','demo_logo.png',NULL,NULL);
