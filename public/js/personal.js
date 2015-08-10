10218aba750d0659f50542dfc5249d924b469c0e
happyhour7@163.com <happyhour7@163.com>
2015-08-10 18:32:12 +0800

    home

---
+++ public/js/personal.js
@@ -0,0 +1,13 @@
+var page_personal={
+	load:function(){
+		$("#btnSub_upload").click(function(){
+			$("#user-photo-upload-form").submit();
+		});
+
+		$("#personal-msg-post-button").click(function(){
+			$("#personal_update_form").submit();
+		});
+	}
+};
+
+window.onload=page_personal.load;
