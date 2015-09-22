62313888fecdf3a472bf5b51a58e189c5e0726db
Rock Zhang <tttt6399998@126.com>
2015-09-23 01:24:26 +0800

    修复发表秘密页面的一些样式

--- public/js/public.js
+++ public/js/public.js
@@ -12,7 +12,6 @@ Date.prototype.format = function(format){
     if(/(y+)/.test(format)) { 
         format = format.replace(RegExp.$1, (this.getFullYear()+"").substr(4 - RegExp.$1.length)); 
     } 
-    console.log(o);
     for(var k in o) { 
         if(new RegExp("("+ k +")").test(format)) { 
             format = format.replace(RegExp.$1, RegExp.$1.length==1 ? o[k] : ("00"+ o[k]).substr((""+ o[k]).length)); 
