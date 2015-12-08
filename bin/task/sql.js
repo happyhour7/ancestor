216b22f3d4878b8cf5aafa15f8beaf453f7821d5
Rock Zhang <tttt6399998@126.com>
2015-12-09 01:06:11 +0800

    '修复长文显示的bug，由sql语句错误导致

--- bin/task/sql.js
+++ bin/task/sql.js
@@ -64,9 +64,30 @@ module.exports.personalAvgGetSQL = "select username, round(avg(agv.avgScore),1) 
 "where username in (<username>) "+
 "group by username";
 
-module.exports.longStoreSQL="select * from files  where secretLimit=1 and islongstory=1 order by id  desc";
+module.exports.longStoreSQL="select *, rp.commNum as commNum,files.*,users.sex as authorSex, users.age as authorAge, users.cityname as authorCity,users.userPhoto as authorPhoto,ig.goodNum as goodNum,ib.badNum as badNum, " +
+    "isgood.good as choosenGood,isbad.bad as choosenBad ,score.score as userscore , round(agv.avgScore,1) as avgscore "+
+    "from files  "+
+    "left join (select fileid,good,count(isgood.good) as goodNum from isgood group by fileid) ig on files.Id=ig.fileid "+
+    "left join (select fileid,bad,count(isbad.bad) as badNum from isbad group by fileid) ib on files.Id=ib.fileid "+
+    "left join (select fileid,count(replay.replayId) as commNum from replay group by fileid) rp on files.Id=rp.fileid "+
+    "left join isgood on files.Id=isgood.fileid and isgood.username='<username>' "+
+    "left join isbad on files.Id=isbad.fileid and isbad.username='<username>' "+
+    "left join score on files.Id=score.fileid and score.username='<username>' "+
+    "left join agvscore as agv on files.Id=agv.fileid "+
+    "left join users on users.username=files.owner "+
+    "where secretLimit=1 and islongstory=1 order by id  desc";
 module.exports.loginLongStoreSQL=
-    "select * from files left join isgood on files.id=isgood.fileid and isgood.username='<username>'  "+
+    "select *, rp.commNum as commNum,files.*,users.sex as authorSex, users.age as authorAge, users.cityname as authorCity,users.userPhoto as authorPhoto,ig.goodNum as goodNum,ib.badNum as badNum, " +
+    "isgood.good as choosenGood,isbad.bad as choosenBad ,score.score as userscore , round(agv.avgScore,1) as avgscore "+
+    "from files  "+
+    "left join (select fileid,good,count(isgood.good) as goodNum from isgood group by fileid) ig on files.Id=ig.fileid "+
+    "left join (select fileid,bad,count(isbad.bad) as badNum from isbad group by fileid) ib on files.Id=ib.fileid "+
+    "left join (select fileid,count(replay.replayId) as commNum from replay group by fileid) rp on files.Id=rp.fileid "+
+    "left join isgood on files.Id=isgood.fileid and isgood.username='<username>' "+
+    "left join isbad on files.Id=isbad.fileid and isbad.username='<username>' "+
+    "left join score on files.Id=score.fileid and score.username='<username>' "+
+    "left join agvscore as agv on files.Id=agv.fileid "+
+    "left join users on users.username=files.owner "+
     "where (((secretLimit<3 or owner='<username>') and secretLimit<>2) or "+
     "(secretLimit=2 and owner in(select friendname from friends where username='<username>'))) "+
     "and islongstory=1  order by id  desc"
