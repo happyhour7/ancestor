d8ebeb41ef04508a56549b7351402611c612c30c
Rock Zhang <iamgtskk@gmail.com>
2016-04-25 23:28:06 +0800

    修复直接访问数据库会报未选择数据库的bug

--- bin/db/connect.js
+++ bin/db/connect.js
@@ -1,113 +1,124 @@
-var mysql = require('mysql');  
-      
+var mysql = require('mysql');
+
 var TEST_DATABASE = 'ancestor';
 var client;
 
 function handleConnect() {
     //创建连接
-    client =mysql.createConnection({
+    client = mysql.createConnection({
         user: 'root',
         password: '6399998'
     });
 
-    client.on('error', function (err) {
+    client.on('error', function(err) {
         console.log('数据库连接出错啦', err);
         // 如果是连接断开，自动重新连接
         //if (err.code === 'PROTOCOL_CONNECTION_LOST') {
-        client =mysql.createConnection({
+        client = mysql.createConnection({
             user: 'root',
             password: '6399998'
         });
     });
 }
 
-var hasOpen=false;
+var hasOpen = false;
 
-module.exports.test=function(){
-  if(hasOpen===false){
+module.exports.test = function() {
+    if (hasOpen === false) {
         open();
     }
-  var sql="insert into users set username=?,password=?,sex=?,usertype=?,score=?,email=?,cityname=?,qq=?,phone=?";
-  //client.query(sql,['william wang','123456',1,1,10,'happyhour7@163.com','南京','251619896','13813994215']);
+    var sql = "insert into users set username=?,password=?,sex=?,usertype=?,score=?,email=?,cityname=?,qq=?,phone=?";
+    //client.query(sql,['william wang','123456',1,1,10,'happyhour7@163.com','南京','251619896','13813994215']);
 }
 
-module.exports.update=function(sql,fn){
-    if(hasOpen===false){
+module.exports.update = function(sql, fn) {
+    if (hasOpen === false) {
         open();
     }
-    if(sql==""){
-      fn.apply({},[""]);
-      return;
+    if (sql == "") {
+        fn.apply({}, [""]);
+        return;
     }
-    client.query(  
-      sql,  
-      function selectCb(err, results, fields) {  
-       /* if (err) {
+    client.query(
+        sql,
+        function selectCb(err, results, fields) {
+            /* if (err) {
           throw err;  
         }  */
-            
-        if(results)
-        {
-         fn.apply(results,[fields]);
-        }    
-      }  
+
+            if (results) {
+                fn.apply(results, [fields]);
+            }
+        }
     );
 }
 
-module.exports.query=function(sql,fn,logic,keyname){
-    if(hasOpen===false){
+module.exports.query = function(sql, fn, logic, keyname) {
+    if (hasOpen === false) {
         open();
     }
-    if(sql==""){
-    	fn.apply({},["",logic]);
-    	return;
+    if (sql == "") {
+        fn.apply({}, ["", logic]);
+        return;
     }
-    client.query(  
-      sql,  
-      function selectCb(err, results, fields) {  
-        /*if (err) {
+    client.query(
+        sql,
+        function selectCb(err, results, fields) {
+            /*if (err) {
           throw err;
         }  */
-            
-        if(results)
-        {
-         fn.apply(results,[keyname,logic]);
-        }    
-      }  
+
+            if (results) {
+                fn.apply(results, [keyname, logic]);
+            }
+        }
     );
 };
-module.exports.execute=function(sql,columns){
-    if(hasOpen===false){
+module.exports.execute = function(sql, columns) {
+    if (hasOpen === false) {
         open();
     }
-    client.query(  
-      sql, 
-      columns  
+    client.query(
+        sql,
+        columns
     );
 };
 
 module.exports.exec = function(sql, datas, callback) {
-  if(hasOpen ===false) {
-    open();
+  if (hasOpen === false) {
+    client.connect(function(err) {
+      if (err) {
+          console.log('error when connecting to db:', err);
+          setTimeout(handleConnect, 2000);
+      } else {
+          client.query("use " + TEST_DATABASE, function(e) {
+            client.query(sql, datas, callback);
+          });
+          hasOpen = true;
+      }
+    });
+  } else {
+    client.query(sql, datas, callback);
   }
-  client.query(sql, datas, callback);
+  
 };
-function open(){
-    client.connect(function (err) {
+
+function open() {
+    client.connect(function(err) {
         if (err) {
             console.log('error when connecting to db:', err);
-            setTimeout(handleConnect , 2000);
+            setTimeout(handleConnect, 2000);
         } else {
             client.query("use " + TEST_DATABASE);
-            hasOpen=true;
+            hasOpen = true;
         }
     });
 }
 
-function close(){
-  return;
+function close() {
+    return;
     client.end();
-    hasOpen=false;
+    hasOpen = false;
 }
 
 handleConnect();
